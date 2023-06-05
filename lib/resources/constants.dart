3b071a118e210ab4e9362bc34f6f3d2aff7a96ff
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-06-06 00:19:46 +0300

    date and time format

--- lib/resources/constants.dart
+++ lib/resources/constants.dart
@@ -3,6 +3,11 @@ const String appName = 'Movies';
 const double smallBorderRadius = 8;
 const double bigBorderRadius = 16;
 
+// Icons
+const double smallIconSize = 20;
+const double mediumIconSize = 30;
+const double largeIconSize = 40;
+
 // Headers Sizes
 const double sizeH1 = 26.0;
 const double sizeH2 = 24.0;
@@ -13,3 +18,23 @@ const double sizeH6 = 14.0;
 const double sizeH7 = 12.0;
 const double sizeH8 = 10.0;
 const double sizeH9 = 8.0;
+
+// Date format
+const List<String> monthsText = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+String formatDateTimeA(DateTime dateTime) {
+  return '${dateTime.year} ${monthsText[dateTime.month]} ${dateTime.day}';
+}
