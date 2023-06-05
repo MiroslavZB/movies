9de7562bf1586793837656d444588741427acfa9
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-06-06 00:19:36 +0300

    details widget

--- lib/pages/details_page.dart
+++ lib/pages/details_page.dart
@@ -9,9 +9,67 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<MapEntry<String, dynamic>> details = movie.toJson().entries.where((e) {
+      dynamic v = e.value;
+      if (v == null) return false;
+      if (v is String) return v.isNotEmpty;
+      if (v is List) return v.isNotEmpty;
+      return true;
+    }).toList();
     return Scaffold(
+      backgroundColor: primaryColor.shade800,
       appBar: AppBar(
         title: Text(movie.title, style: styleH3Bold),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Divider(
+            color: onPrimaryColor,
+            thickness: 3,
+          ),
+          if (movie.date != null)
+            detailEntry(
+              MapEntry(
+                'Release date',
+                formatDateTimeA(movie.date!),
+              ),
+            ),
+          if (movie.duration_ != null)
+            detailEntry(
+              MapEntry(
+                'Duration',
+                '${movie.duration_!.inMinutes} minutes',
+              ),
+            ),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: details.length,
+              itemBuilder: (_, i) => detailEntry(details[i]),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget detailEntry(MapEntry<String, dynamic> e) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: '${e.key}: ',
+              style: styleH4,
+            ),
+            TextSpan(
+              text: (e.value is List) ? e.value.join(', ') : e.value.toString(),
+              style: styleH3Bold,
+            ),
+          ],
+        ),
       ),
     );
   }
