6850f961d2ace1d1c99718e9c6d2bdbbfcf88330
Miroslav Blagoev <miroslav.z.blagoev@gmail.com>
2023-06-06 21:23:02 +0300

    converted to stateless, minified code, fixed an edge case happening pre-fetch finish

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,17 +1,11 @@
 import 'package:movies/components/movie_card.dart';
 import 'package:movies/controllers/user_controller.dart';
-import 'package:movies/pages/home_page.dart';
 import 'package:movies/resources/utils.dart';
 import 'package:provider/provider.dart';
 
-class AccountPage extends StatefulWidget {
+class AccountPage extends StatelessWidget {
   const AccountPage({Key? key}) : super(key: key);
 
-  @override
-  State<AccountPage> createState() => _AccountPageState();
-}
-
-class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,67 +17,85 @@ class _AccountPageState extends State<AccountPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              ElevatedButton(
-                onPressed: () => context.pushReplacement(Paths.auth),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: const [
-                    Text('Exit', style: styleH4Bold),
-                    Padding(
-                      padding: EdgeInsets.only(left: 5),
-                      child: Icon(Icons.logout_outlined),
-                    ),
-                  ],
-                ),
-              ),
+              exitButton(context),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  'My List:',
+                  'Saved to My List:',
                   style: styleH1Bold.copyWith(color: primaryColor),
                 ),
               ),
               Consumer<UserController>(
                 builder: (_, controller, __) {
                   if (controller.savedIndexes.isEmpty) {
-                    return Row(
-                      children: [
-                        Text(
-                          'Nothing here.',
-                          style: styleH4.copyWith(color: primaryColor),
-                        ),
-                        InkWell(
-                          onTap: () => context.pushReplacement(Paths.home),
-                          child: Text(
-                            ' Add?',
-                            style: styleH4Bold.copyWith(
-                              color: primaryColor,
-                              decoration: TextDecoration.underline,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
+                    return emptyAndRedirect(context);
                   }
-                  return ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxHeight: MediaQuery.of(context).size.height * 0.8,
-                    ),
-                    child: ListView.builder(
-                      itemCount: controller.savedIndexes.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (_, i) => MovieCard(
-                        movie: movies.firstWhere(
-                          (e) => e.id == controller.savedIndexes[i],
-                        ),
-                      ),
-                    ),
-                  );
+                  return bookmarkedMovies(context, controller);
                 },
               )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  // Widgets
+  Widget exitButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => context.pushReplacement(Paths.auth),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Text('Exit', style: styleH4Bold),
+          Padding(
+            padding: EdgeInsets.only(left: 5),
+            child: Icon(Icons.logout_outlined),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget emptyAndRedirect(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          'Nothing here.',
+          style: styleH4.copyWith(color: primaryColor),
+        ),
+        InkWell(
+          onTap: () => context.pushReplacement(Paths.home),
+          child: Text(
+            ' Add?',
+            style: styleH4Bold.copyWith(
+              color: primaryColor,
+              decoration: TextDecoration.underline,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget bookmarkedMovies(BuildContext context, UserController controller) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.8,
+      ),
+      child: ListView.builder(
+        itemCount: controller.savedIndexes.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (_, i) {
+          if (controller.movies.where((e) => e.id == controller.savedIndexes[i]).isEmpty) {
+            return Container();
+          }
+          return MovieCard(
+            movie: controller.movies.firstWhere(
+              (e) => e.id == controller.savedIndexes[i],
+            ),
+          );
+        },
       ),
     );
   }
